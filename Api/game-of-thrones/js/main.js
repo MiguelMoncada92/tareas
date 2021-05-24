bf9c0ce87ea76f06583fc54486a6dad2de9df83b
Miguel Moncada <kewpul@gmail.com>
2021-05-24 13:25:52 -0400

    game of Thrones

--- Api/game-of-thrones/js/main.js
+++ Api/game-of-thrones/js/main.js
@@ -1,6 +1,7 @@
-var url = "https://anapioficeandfire.com/api/houses?page=1&pageSize=200";
+var url = "https://anapioficeandfire.com/api/houses?page=2&pageSize=200";
+
 $(document).ready(function() {
     $.get(url, function(respuesta) {
-        console.log(respuesta);
+        console.log(url);
     }, "json")
 });
