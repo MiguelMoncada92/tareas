b14f0f94ed5abcad1b65e5aa52ac94262ebd7465
Miguel Moncada <kewpul@gmail.com>
2021-05-24 13:27:03 -0400

    game of thrones

--- Api/game-of-thrones/js/main.js
+++ Api/game-of-thrones/js/main.js
@@ -1,7 +1,34 @@
-var url = "https://anapioficeandfire.com/api/houses?page=2&pageSize=200";
+var url = "https://anapioficeandfire.com/api/houses";
 
+function nombreCasa(a) {
+    $.get(url + "/" + a + "/", function(respuesta) {
+        var innerHTML = "";
+        if (a == 16) {
+            innerHTML += "<h3>Casa:</h3> <h4>" + respuesta.name + "</h4>"
+            innerHTML += "<h3>Region: </h3><h4>" + respuesta.region + "</h4>"
+            innerHTML += "<h3>Titulos: </h3><h4>" + respuesta.titles + "</h4>"
+        } else {
+            innerHTML += "<h3>Casa:</h3> <h4>" + respuesta.name + "</h4>"
+            innerHTML += "<h3>Region: </h3><h4>" + respuesta.region + "</h4>"
+            innerHTML += "<h3>Palabras: </h3><h4>" + respuesta.words + "</h4>"
+            innerHTML += "<h3>Titulos: </h3><h4>" + respuesta.titles + "</h4>"
+        }
+        $(".descripcion").html(innerHTML);
+    }, "json");
+};
+
+function dejarEnBlanco() {
+    $(".descripcion").html("<h4> Mueve el mouse sobre las imágenes </h4>");
+};
 $(document).ready(function() {
-    $.get(url, function(respuesta) {
-        console.log(url);
-    }, "json")
+    $(".descripcion").hide();
+    $(".descripcion").fadeIn(1000);
+    dejarEnBlanco();
+    $("img").hover(function() {
+            var numeroCasa = $(this).attr("info-src");
+            nombreCasa(numeroCasa);
+        },
+        function() {
+            dejarEnBlanco();
+        });
 });
