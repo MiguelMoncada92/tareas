1d98673dd487122a9771d2a6437f7b3d6c9ce425
Miguel Moncada <kewpul@gmail.com>
2021-06-07 11:04:21 -0400

    Primer programa java

---
+++ Java/PrimerPrograma/src/PrimerPrograma.java
@@ -0,0 +1,13 @@
+
+public class PrimerPrograma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Mi nombre es Miguel");
+		System.out.println("Tengo 29 años de edad");
+		System.out.println("Mi ciudad es Loncoche City");
+		
+		
+	}
+
+}
