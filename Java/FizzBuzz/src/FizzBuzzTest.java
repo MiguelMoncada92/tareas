5b67e40c36c4c96a4156eb97041ca2bfb03e0476
Miguel Moncada <kewpul@gmail.com>
2021-06-07 15:15:59 -0400

    FizzBuzz

---
+++ Java/FizzBuzz/src/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		FizzBuzz iD = new FizzBuzz();
+		String FB = iD.fizzBuzz(15);
+		System.out.println(FB);
+		
+
+	}
+
+}
