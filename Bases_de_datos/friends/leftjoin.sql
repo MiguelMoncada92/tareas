80d01272296148b0ecdf39cff6d5274e80eb5820
Miguel Moncada <kewpul@gmail.com>
2021-06-11 16:41:05 -0400

    friendship

--- Bases_de_datos/friends/leftjoin.sql
+++ Bases_de_datos/friends/leftjoin.sql
@@ -1,4 +1,35 @@
-SELECT users.first_name, users.last_name, friendships.user_id, friendships.friend_id
+SELECT CONCAT(users.first_name," ", users.last_name)AS first, CONCAT (users2.first_name, " ", users2.last_name) AS friend_name
 FROM users 
-LEFT JOIN friendships ON users.id = friendships.user_id 
+left JOIN friendships ON users.id = friendships.user_id 
+JOIN users AS users2 ON friendships.friend_id = users2.id
+
+
+SELECT CONCAT(users.first_name," ", users.last_name)AS first, CONCAT (users2.first_name, " ", users2.last_name) AS friend_name
+FROM users 
+left JOIN friendships ON users.id = friendships.user_id 
+JOIN users AS users2 ON friendships.friend_id = users2.id
+WHERE users.first_name = "Kermit" OR users2.first_name = "Kermit"
+
+SELECT count(friendships.id) AS "Numer of friendships" FROM friendships 
+
+SELECT COUNT(friendships.user_id),users.first_name,users.last_name, users2.first_name,users2.last_name
+FROM users 
+left JOIN friendships ON users.id = friendships.user_id 
+JOIN users AS users2 ON friendships.friend_id = users2.id
+
+-- INSERT INTO users (first_name, last_name, created_at, updated_at)
+-- VALUES ("Juana", "De Arco", NOW(), NOW())
+
+-- INSERT INTO friendships (user_id, friend_id, created_at, updated_at )
+-- VALUES (6, 2, NOW(), NOW())
+-- INSERT INTO friendships (user_id, friend_id, created_at, updated_at )-- VALUES (6, 4, NOW(), NOW())
+-- INSERT INTO friendships (user_id, friend_id, created_at, updated_at )
+-- VALUES (6, 5, NOW(), NOW())
+
+-- DELETE FROM friendships WHERE friendships.id=5
+
+SELECT users.first_name, users.last_name, users2.first_name AS "friend_first_name", users2.last_name AS "friend_last_name"
+FROM users 
+left JOIN friendships ON users.id = friendships.user_id 
+JOIN users AS users2 ON friendships.friend_id = users2.id
 
