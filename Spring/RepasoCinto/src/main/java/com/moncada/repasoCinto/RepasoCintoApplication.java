a2332ab71f15e2df724d66c9c96233109f4bc12a
Miguel Moncada <kewpul@gmail.com>
2021-07-27 21:05:59 -0400

    examencintorepaso

---
+++ Spring/RepasoCinto/src/main/java/com/moncada/repasoCinto/RepasoCintoApplication.java
@@ -0,0 +1,13 @@
+package com.moncada.repasoCinto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoCintoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoCintoApplication.class, args);
+	}
+
+}
---
+++ Spring/RepasoCinto/src/test/java/com/moncada/repasoCinto/RepasoCintoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moncada.repasoCinto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoCintoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
