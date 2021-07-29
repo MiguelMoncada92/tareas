b33a96d78ff8fb616fde73960e683106d7ccee4c
Miguel Moncada <kewpul@gmail.com>
2021-07-28 20:53:21 -0400

    security

---
+++ Spring/PanelAdmin/src/test/java/com/moncada/panelAdmin/PanelAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moncada.panelAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
